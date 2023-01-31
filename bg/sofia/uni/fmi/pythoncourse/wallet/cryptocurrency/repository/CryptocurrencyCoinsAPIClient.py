0051f12ae957fac71d19885ade1c0ad29bb0665f
Stiliyan Iliev <stiliyaniliev00@gmail.com>
2023-01-31 11:06:18 +0200

    Added User interface and APIClient for communication with REST API.

---
+++ bg/sofia/uni/fmi/pythoncourse/wallet/cryptocurrency/repository/CryptocurrencyCoinsAPIClient.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+import requests
+from requests import Response
+
+
+class CryptocurrencyCoinsAPIClient:
+    @staticmethod
+    def get_specific_rate_of_currency(asset_id_base: str, asset_id_quote: str) -> Response:
+        url = 'https://rest.coinapi.io/v1/exchangerate/' + asset_id_base + '/' + asset_id_quote
+        headers = {'X-CoinAPI-Key': 'F5DFB3A6-F7DA-4645-8DDA-A8D713C18129'}
+        return requests.get(url, headers=headers)
+
+    @staticmethod
+    def get_all_current_rates_of_currency(asset_id_base: str) -> Response:
+        url = 'https://rest.coinapi.io/v1/exchangerate/' + asset_id_base + '?invert=false'
+        headers = {'X-CoinAPI-Key': 'F5DFB3A6-F7DA-4645-8DDA-A8D713C18129'}
+        return requests.get(url, headers=headers)
+
+    @staticmethod
+    def get_currency_historical_exchange_rates(asset_id_base: str, asset_id_quote: str, period_id: str,
+                                               time_start: datetime, time_end: datetime) -> Response:
+        url = 'https://rest.coinapi.io/v1/exchangerate/' + asset_id_base + '/' + asset_id_quote + '/' + \
+              'history?period_id=' + period_id + '&ime_start=' + time_start.strftime('%Y-%m-%dT%H:%M:%S') + \
+              '&time_end=' + time_end.strftime('%Y-%m-%dT%H:%M:%S')
+        'BTC/USD/history?period_id=1MIN&time_start=2016-01-01T00:00:00&time_end=2016-02-01T00:00:00'
+        headers = {'X-CoinAPI-Key': '73034021-THIS-IS-SAMPLE-KEY'}
+        return requests.get(url, headers=headers)
---
+++ bg/sofia/uni/fmi/pythoncourse/wallet/cryptocurrency/user/User.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+from typing import Dict
+
+
+class User(ABC):
+
+    @abstractmethod
+    def deposit_money(self, amount: float):
+        """
+        Allows the current user to top up a certain amount of USA dollars into his/her wallet.
+        @param amount: The amount of money which the current user wants to add to his/her wallet.
+        """
+        pass
+
+    @abstractmethod
+    def list_offerings(self) -> Dict[str, float]:
+        """
+        Allow the current user to see all available cryptocurrencies at the moment, which he/she may or may not
+        want to buy. The information is from CoinAPI (https://www.coinapi.io).
+        @return Returns a dictionary of all currently available cryptocurrencies and their prices.
+        """
+        pass
+
+    @abstractmethod
+    def buy(self, offering_code: str, amount: float):
+        """
+        Allows the current user to bull a certain amount of a certain cryptocurrency, if there is enough deposit money
+        in his/her wallet.
+        @param offering_code: The code of the cryptocurrency which the current user wants to buy.
+        @param amount: The amount of the cryptocurrency which the current user wants to buy.
+        """
+        pass
+
+    @abstractmethod
+    def sell(self, offering_code: str):
+        """
+        Allows the current user to sell all the amount of a certain cryptocurrency.
+        @param offering_code: The code of the cryptocurrency which the current user wants to sell.
+        """
+        pass
+
+    @abstractmethod
+    def is_valid_password(self, password: str) -> bool:
+        """
+        @param password: The currently inputted password.
+        @return: true if the value of password1 equals the current user's password value
+        """
+        pass
